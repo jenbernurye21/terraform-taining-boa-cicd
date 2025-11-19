b1ba6b356ed75bc753387dbdf2c4ddd9a855f79a
jenbernurye21 <nuryejenber@gmail.com>
2025-11-19 15:08:37 +0000

    add .gitignore

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "example" {
   ami           = var.my-os-image
   instance_type = var.machine-size
   key_name      = var.private_key_name
+  vpc_security_group_ids = [aws_security_group.jenber_sec-groups.id]
   tags = {
     Name = var.vm_name
   }
--- security-group.tf
+++ security-group.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "allow_tls" {
   vpc_id      = data.aws_vpc.jenber_existing_vpc.id 
 
   tags = {
-    Name = "jenber_firewall_rules"
+    Name = "jenber_firewall_rules _final"
   }
 }
 #allow all outgoing groups
@@ -13,21 +13,21 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
-#allow 22 and 88 
+#allow 22 from  
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
-  from_port         = 443
+  security_group_id = aws_security_group.jenber_sec-groups.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 22
 }
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
-  from_port         = 443
+## allows 80 port from everywhere
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_http" {
+  security_group_id = aws_security_group.jenber_sec-groups.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 88
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 88
 }
 
 data "aws_vpc" "jenber_existing_vpc" {
